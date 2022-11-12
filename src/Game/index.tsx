9bea4ed4d5b5087c0ae8dd4cdda166f4a8ff0cbe
na2na-p <na2na@na2na.dev>
2022-11-12 09:54:33 +0900

    Update Button import

--- src/Game/index.tsx
+++ src/Game/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Button from '@mui/material/Button';
+import Button from '@/components/Button';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 
@@ -9,13 +9,13 @@ import DataDisplay from './DataDisplay';
 import useHooks from './useHooks';
 
 export default () => {
-	const { histories, setResultHistory, onResetButtonClick} = useHooks();
-    return (
-        <Stack spacing={2}>
-            <Input setter={setResultHistory} />
-						<Divider />
-						<DataDisplay histories={histories} />
-						<Button variant='contained' onClick={onResetButtonClick}>もう一度最初から</Button>
-        </Stack>
-    )
-}
+	const { histories, setResultHistory, onResetButtonClick } = useHooks();
+	return (
+		<Stack spacing={2}>
+			<Input setter={setResultHistory}/>
+			<Divider/>
+			<DataDisplay histories={histories}/>
+			<Button onClick={onResetButtonClick}>もう一度最初から</Button>
+		</Stack>
+	);
+};
