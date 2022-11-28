588407b0b03d96b244d03afff28b7d10f0233d55
na2na-p <na2na@na2na.dev>
2022-11-28 16:39:33 +0900

    setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()]
+	plugins: [react(), tsconfigPaths()],
+	test: {
+		globals: true,
+		environment: 'happy-dom',
+		setupFiles: './test/setup.ts'
+	}
 });
