2da746a538ab8edfb81a47f29e01297d86ff4b31
na2na-p <na2na@na2na.dev>
2022-11-28 16:43:01 +0900

    Update

--- src/Game/index.tsx
+++ src/Game/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Button from '@/components/input/Button';
 import Stack from '@/components/layout/Stack';
 import Divider from '@/components/layout/Divider';
+import Typography from '@/components/Typography';
 
 import Input from './Input';
 import DataDisplay from './DataDisplay';
@@ -12,6 +13,8 @@ export default () => {
 	const { histories, setResultHistory, onResetButtonClick } = useHooks();
 	return (
 		<Stack spacing={2}>
+			<Typography variant="h4">数当てゲーム</Typography>
+			<Divider />
 			<Input setter={setResultHistory} />
 			<Divider />
 			<DataDisplay histories={histories} />
