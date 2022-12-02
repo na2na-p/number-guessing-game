915a86cf266b1b29c2f07ea69b12a6a91a301372
na2na-p <na2na@na2na.dev>
2022-12-02 15:39:24 +0900

    Add 動作テスト

--- src/Game/Game.test.tsx
+++ src/Game/Game.test.tsx
@@ -2,10 +2,46 @@
 import { describe, expect, test } from 'vitest';
 import Game from './';
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
-describe('Game root component', () => {
-	test('should render correctly', () => {
+describe('Game component', () => {
+	test('レンダリングテスト', () => {
 		const { container } = render(<Game />);
 		expect(container).toMatchSnapshot();
+	});
+
+	describe('数当てテスト', () => {
+		test('正解した場合', () => {
+			render(<Game answer={20} />);
+			const input: HTMLInputElement = screen.getByPlaceholderText('いくつでしょう?');
+			const button: HTMLButtonElement = screen.getByRole('button', { name: 'トライ!' });
+			act(() => {
+				input.value = '20';
+				button.click();
+			});
+			expect(screen.getByText('正解')).toBeInTheDocument();
+		});
+
+		test('数が大きすぎた場合', () => {
+			render(<Game answer={20} />);
+			const input: HTMLInputElement = screen.getByPlaceholderText('いくつでしょう?');
+			const button: HTMLButtonElement = screen.getByRole('button', { name: 'トライ!' });
+			act(() => {
+				input.value = '30';
+				button.click();
+			});
+			expect(screen.getByText('もっと小さい')).toBeInTheDocument();
+		});
+
+		test('数が小さすぎた場合', () => {
+			render(<Game answer={20} />);
+			const input: HTMLInputElement = screen.getByPlaceholderText('いくつでしょう?');
+			const button: HTMLButtonElement = screen.getByRole('button', { name: 'トライ!' });
+			act(() => {
+				input.value = '10';
+				button.click();
+			});
+			expect(screen.getByText('もっと大きい')).toBeInTheDocument();
+		});
 	});
 });
